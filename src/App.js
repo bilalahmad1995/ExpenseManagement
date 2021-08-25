338f546222fcd456201a565c7a4f896d542051c8
Bilal Ahmad <iambilaaal@gmail.com>
2021-08-25 14:17:39 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurace";
+  // const expensePrice = 150;
+
+  const expenses = [
+    {
+      title: "Health Insurance",
+      date: new Date(2021, 2, 28),
+      amount: 50,
+    },
+
+    {
+      title: "Car Insurance",
+      date: new Date(2021, 2, 28),
+      amount: 90,
+    },
+
+    {
+      title: "House Insurance",
+      date: new Date(2021, 2, 28),
+      amount: 80,
+    },
+
+    {
+      title: "Dental Insurance",
+      date: new Date(2021, 2, 28),
+      amount: 28,
+    },
+  ];
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Expense App</h1>
+
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
