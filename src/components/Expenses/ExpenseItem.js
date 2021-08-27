7da8773b5f23b29916282b255fa9c4eee362cfd9
Bilal Ahmad <iambilaaal@gmail.com>
2021-08-27 18:01:19 +0200

    Filter added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,7 +14,6 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button className="btn">Change title</button>
       </div>
     </Card>
   );
